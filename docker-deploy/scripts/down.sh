687a55d8618b9661622ac8c66861085a2c068f69
Sergey Tyapkin <Tyapkin2002@mail.ru>
2026-05-28 21:58:10 +0300

    Fix spripts exit codes

--- docker-deploy/scripts/down.sh
+++ docker-deploy/scripts/down.sh
@@ -1,2 +1,12 @@
-cd docker-deploy || exit
+source ./docker-deploy/scripts/_echo-colors.sh &&
+echo_step "Stopping docker compose..." &&
+
+cd docker-deploy &&
 docker compose --env-file ../.env down
+
+if [ $? -eq 0 ]; then
+    echo_success "Docker containers stopped"
+else
+    echo_error "Errors when stopping docker containers"
+    exit 1
+fi
